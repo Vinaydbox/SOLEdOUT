316c20ba4c247584a5995d321cb118afd168a296
vinay <vinay.v@darwinbox.io>
2023-02-13 14:47:22 +0530

    add to cart

--- js/renderSingleProd.js
+++ js/renderSingleProd.js
@@ -2,5 +2,5 @@ window.onload = function () {
     var id = window.location.hash.substring(1);
     let url = "http://localhost:3003/fetchProdByID/" + id;
     singleProductArea = $("#singleProductArea")
-
+    
 }
