ac34f029d97b45ea97f60015a2a436c56a523ced
ashfaq.p <ashfaq.m@darwinbox.io>
2023-02-21 14:54:39 +0530

    recommendations

--- Backend/MVC/controllers/users/userrecomms.js
+++ Backend/MVC/controllers/users/userrecomms.js
@@ -6,12 +6,14 @@ async function userRecommendations(req, res) {
     console.log(req.body);
     // console.log(req.params.email);
     console.log(req.body.email); 
-    console.log(req.body.onclickRecomms);   
-    user.findOneAndUpdate({email :req.body.email},{$set:{'userRecommendations.onclickRecommendations':req.body.onclickRecomms,'userRecommendations.searchRecommendations':req.body.searchRecomms}},{},(err,docs)=>{
+    console.log(req.body.onclickRecomms);
+
+    user.findOneAndUpdate({email :req.body.email},{$set:{'userRecommendations.onclickRecommendations':req.body.onclickRecomms,'userRecommendations.searchRecommendations':req.body.searchRecomms}},{upsert:true},(err,docs)=>{
         if(err){
             console.log(err);
         }
         else{
+
             res.send("Hero hogaya");
         }
     })
