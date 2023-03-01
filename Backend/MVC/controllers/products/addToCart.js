492f3cc64eafec1751acf1031e8ef0c423bb4ab6
vinay <vinay.v@darwinbox.io>
2023-03-01 23:13:03 +0530

    forhelp

--- Backend/MVC/controllers/products/addToCart.js
+++ Backend/MVC/controllers/products/addToCart.js
@@ -7,50 +7,52 @@ async function addToCart(req, res) {
 	// console.log(req.body.loggedinUserEmail);
 	console.log(req.body.loggedinUserEmail);
 	const doc = await usergen.find({email: req.body.loggedinUserEmail});
-		if (err) {
-			console.log("err");
-		} else {
-			let found = false;
-			doc = data;
-			for (let t = 0; t < doc[0].userCart.length; t++) {
-				if (doc[0].userCart[t].productName === req.body.productName) {
-					doc[0].userCart[t].count++;
-					found = true;
-				}
+	try{
+		let found = false;
+		doc = data;
+		for (let t = 0; t < doc[0].userCart.length; t++) {
+			if (doc[0].userCart[t].productName === req.body.productName) {
+				doc[0].userCart[t].count++;
+				found = true;
 			}
-			console.log(found);
-			if (found == true) {
-				usergen.updateOne({ email: req.body.loggedinUserEmail }, { $set: { userCart: doc[0].userCart } }, { upsert: true }, (err, docs) => {
-					if (err) {
-						console.log(err);
-					}
-					else {
-						console.log(docs);
-					}
-				});
-			}
-			if (found != true) {
-				console.log("coming");
-				let obj = {
-					productName: req.body.productName,
-					price: req.body.price,
-					productURL: req.body.productURL,
-					count: 1,
-					brand: req.body.brand,
-					pid: req.body.pid,
-				}
-				// console.log(obj);
-				usergen.updateOne({ email: req.body.loggedinUserEmail }, { $push: { userCart: obj } }, { upsert: true }, (err, docs) => {
-					if (err) {
-						console.log(err);
-					}
-					else {
-						console.log(docs);
-					}
-				})
-			}
-			res.send("addedToCart");
 		}
+		console.log(found);
+		if (found == true) {
+			usergen.updateOne({ email: req.body.loggedinUserEmail }, { $set: { userCart: doc[0].userCart } }, { upsert: true }, (err, docs) => {
+				if (err) {
+					console.log(err);
+				}
+				else {
+					console.log(docs);
+				}
+			});
+		}
+		if (found != true) {
+			console.log("coming");
+			let obj = {
+				productName: req.body.productName,
+				price: req.body.price,
+				productURL: req.body.productURL,
+				count: 1,
+				brand: req.body.brand,
+				pid: req.body.pid,
+			}
+			// console.log(obj);
+			usergen.updateOne({ email: req.body.loggedinUserEmail }, { $push: { userCart: obj } }, { upsert: true }, (err, docs) => {
+				if (err) {
+					console.log(err);
+				}
+				else {
+					console.log(docs);
+				}
+			})
+		}
+		res.send("addedToCart");
+	}
+	catch(err){
+		res.send(err);
+	}
+	
 }
 
 
