bd707cc307011b50797d8e79bc3a08cbe8a64c77
vinay <vinay.v@darwinbox.io>
2023-03-02 00:09:37 +0530

    done

--- js/urls.js
+++ js/urls.js
@@ -1,6 +1,6 @@
 function head() {
-    let elasticIP = "http://127.0.0.1:3003"
-    // let elasticIP = "http://3.6.140.23:3003/";
+    // let elasticIP = "http://127.0.0.1:3003"
+    let elasticIP = "http://3.6.140.23:3003";
     return elasticIP;
 }
 export default head;
