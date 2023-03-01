f7a83d954e3601bbe996c822f73a47e51aae235a
vinay <vinay.v@darwinbox.io>
2023-03-01 18:02:22 +0530

    async

--- Backend/MVC/controllers/products/fetchall.js
+++ Backend/MVC/controllers/products/fetchall.js
@@ -1,15 +1,21 @@
 const productgen = require('../../models/products/productModel').productModel;
 
 async function fetchAllProducts(req, res) {
-    await productgen.find({}, (err, docs) => {
-        if (err) {
-            res.send("something went wrong");
-        }
-        else {
-            console.log("products milgaye");
-            res.send(JSON.stringify(docs));
-        }
-    })
+    // productgen.find({}, (err, docs) => {
+    //     if (err) {
+    //         res.send("something went wrong");
+    //     }
+    //     else {
+    //         console.log("products milgaye");
+    //         res.send(JSON.stringify(docs));
+    //     }
+    // })
+    try {
+        const data = await productgen.find({});
+        res.send(JSON.stringify(data));
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 module.exports = { fetchAllProducts };
