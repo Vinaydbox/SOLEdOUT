bf83260f262e0f911e3d026f40792adc6147c933
vinay <vinay.v@darwinbox.io>
2023-03-03 12:14:06 +0530

    docker

--- Backend/MVC/controllers/products/decrementProductCount.js
+++ Backend/MVC/controllers/products/decrementProductCount.js
@@ -2,10 +2,11 @@ const productgen = require('../../models/products/productModel').productModel;
 
 async function decrementProductCount(req, res) {
     // console.log(req.body)
-    const det = await productgen.updateOne({ pid: req.params.pid },
-        {
-            $inc: { "count": -parseInt(req.body.count) }
-        })
+    let det = await productgen.updateOne({ pid: req.params.pid }, {
+        $inc: {
+            "count": -parseInt(req.body.count)
+        }
+    })
     res.send("decremented product count")
     // try{
     //     res.send(det);
